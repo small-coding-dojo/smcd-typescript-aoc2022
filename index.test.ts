2692f69f31b7e530ada87f9b17eb24e0ef772dd0
Niklas Rueber <nrueber@igus.net>
2023-11-30 17:15:46 +0100

    feat: single elf with most calories is returned with 0 calories
    Co-authored-by: Stefan Boos <kontakt@boos.systems>
    Co-authored-by: Holger Janssen-Kroll <hjanssenkroll@igus.net>
    Co-authored-by: Niklas Rueber <niklas@rueber.cloud>
    Co-authored-by: Bernhard Schenk <bernhardschenk@gmx.de>
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>

---
+++ index.test.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "bun:test";
+
+const input = `0`;
+
+test("Elf with most calories", () => {
+    var result = elfWithMostCalories(input);
+    
+    expect(result).toBe(0);
+});
+
+function elfWithMostCalories(input: string) {
+    return 0;
+}
