ff3e9619ac9aa004b30d8a5677b127193a4cf6c4
Bernhard Schenk <bernhardschenk@gmx.de>
2023-11-30 17:38:15 +0100

    feat: We can count one elf with one parameterized item
    Co-authored-by: Stefan Boos <kontakt@boos.systems>
    Co-authored-by: Holger Janssen-Kroll <hjanssenkroll@igus.net>
    Co-authored-by: Niklas Rueber <niklas@rueber.cloud>
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>

--- index.test.ts
+++ index.test.ts
@@ -8,6 +8,30 @@ test("Elf with most calories", () => {
     expect(result).toBe(0);
 });
 
+test("Elf can carry 1 item with 1 calory", () => {
+    var result = elfWithArbitraryName('1')
+
+    expect(result).toBe(1)
+})
+
+test("Elf can carry 1 item with 10 calories", () => {
+    var result = elfWithArbitraryName('10')
+
+    expect(result).toBe(10)
+})
+
+test("Elf can carry 1 item with 100 calories", () => {
+    var result = elfWithArbitraryName('100')
+
+    expect(result).toBe(100)
+})
+
+
+
 function elfWithMostCalories(input: string) {
-    return 0;
+    return 0
+}
+
+function elfWithArbitraryName(input: string) {
+    return parseInt(input)
 }
